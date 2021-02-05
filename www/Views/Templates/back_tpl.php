1c2e528c1a1e29d498865b164525ef99a71b7401
maximecarl <mcarluer@myges.fr>
2021-02-05 18:09:32 +0100

    Integration Init

--- www/Views/Templates/back_tpl.php
+++ www/Views/Templates/back_tpl.php
@@ -1,18 +1,68 @@
 <!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<meta charset="UTF-8">
-		<title>Template de Back</title>
-		<meta name="description" content="ceci est la page de template">
-	</head>
-	<body>
-		<header>
-			<h1>Template Backoffice</h1>
-		</header>
+<html>
+<head>
+    <meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Back Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" type="text/css" href=<?php App\Core\View::getAssets("main.css")?>>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
-		<!-- intégrer le vue -->
-		<?php include $this->view ?>
-
-	</body>
+    <script>
+        function toggleMenu(){
+            document.getElementById('back-mainPage-menuResponsive').classList.toggle('hidden');
+        }
+    </script>
+</head>
+<body>
+    <header>
+        <img src=<?php App\Core\View::getAssets("odyssey_logo_v2.svg")?> alt="Odyssey" class="back-mainPage-header-logo">
+        <p class="back-mainPage-header-websiteName">Mon projet annuel Web</p>
+        <div class="back-mainPage-header-actionContainer">
+            <button onclick="alert('Accès au site non disponible')" class="fullButton"><img src=<?php App\Core\View::getAssets("icon_web.png")?> alt="Accès au site" class="iconWhite"></button>
+            <button onclick="toggleMenu()" class="fullButton d-inline-block d-lg-none"><img src=<?php App\Core\View::getAssets("icon_menu.png")?> alt="Menu" class="iconWhite"></button>
+        </div>
+    </header>
+    <nav id="back-mainPage-menu" class="d-none d-lg-flex">
+        <ul>
+            <li class="selected"><a href="#tableau-de-bord"><img src=<?php App\Core\View::getAssets("icon_home.png")?> alt="" class="icon iconWhite">Tableau de bord</a></li>
+            <li><a href="#articles"><img src=<?php App\Core\View::getAssets("icon_page.png")?> alt="" class="icon iconWhite">Articles</a></li>
+            <li><a href="#utilisateurs"><img src=<?php App\Core\View::getAssets("icon_user.png")?> alt="" class="icon iconWhite">Utilisateurs</a></li>
+            <li><a href="#commentaires"><img src=<?php App\Core\View::getAssets("icon_comment.png")?> alt="" class="icon iconWhite">Commentaires</a></li>
+        </ul>
+        <ul>
+            <li><a href="#templates"><img src=<?php App\Core\View::getAssets("icon_page.png")?> alt="" class="icon iconWhite">Templates</a></li>
+            <li><a href="#gestion-menu"><img src=<?php App\Core\View::getAssets("icon_menu.png")?> alt="" class="icon iconWhite">Gestion du menu</a></li>
+            <li><a href="#newsletter"><img src=<?php App\Core\View::getAssets("icon_newsletter.png")?> alt="" class="icon iconWhite">Newsletter</a></li>
+        </ul>
+        <ul>
+            <li><a href="#acces-site"><img src=<?php App\Core\View::getAssets("icon_web.png")?> alt="" class="icon iconWhite">Accès au site</a></li>
+            <li><a href="#parametres"><img src=<?php App\Core\View::getAssets("icon_settings.png")?> alt="" class="icon iconWhite">Paramètres</a></li>
+        </ul>
+    </nav>
+    <nav id="back-mainPage-menuResponsive" class="d-block d-lg-none hidden">
+        <ul>
+            <li class="selected"><a href="#tableau-de-bord"><img src=<?php App\Core\View::getAssets("icon_home.png")?> alt="" class="icon iconWhite">Tableau de bord</a></li>
+            <li><a href="#articles"><img src=<?php App\Core\View::getAssets("icon_page.png")?> alt="" class="icon iconWhite">Articles</a></li>
+            <li><a href="#utilisateurs"><img src=<?php App\Core\View::getAssets("icon_user.png")?> alt="" class="icon iconWhite">Utilisateurs</a></li>
+            <li><a href="#commentaires"><img src=<?php App\Core\View::getAssets("icon_comment.png")?> alt="" class="icon iconWhite">Commentaires</a></li>
+        </ul>
+        <ul>
+            <li><a href="#templates"><img src=<?php App\Core\View::getAssets("icon_page.png")?> alt="" class="icon iconWhite">Templates</a></li>
+            <li><a href="#gestion-menu"><img src=<?php App\Core\View::getAssets("icon_menu.png")?> alt="" class="icon iconWhite">Gestion du menu</a></li>
+            <li><a href="#newsletter"><img src=<?php App\Core\View::getAssets("icon_newsletter.png")?> alt="" class="icon iconWhite">Newsletter</a></li>
+        </ul>
+        <ul>
+            <li><a href="#acces-site"><img src=<?php App\Core\View::getAssets("icon_web.png")?> alt="" class="icon iconWhite">Accès au site</a></li>
+            <li><a href="#parametres"><img src=<?php App\Core\View::getAssets("icon_settings.png")?> alt="" class="icon iconWhite">Paramètres</a></li>
+        </ul>
+    </nav>
+    <main id="back-mainPage-mainContent">
+        <div id="back-manPage-gridContent" class="d-flex-wrap d-lg-grid">
+			<?php include $this->view ?>
+        </div>
+    </main>
+</body>
 </html>
--- www/Views/dashboard_view.php
+++ www/Views/dashboard_view.php
@@ -1,3 +1,311 @@
-<section>
-	<h2>Page du dashboard</h2>
+<!-- Line 1 -->
+<section id="dashboard-section-statistics" class="col-6" style="grid-column: 1 / 7; grid-row: 1;">
+	<h1 class="titleSection"><img src=<?php App\Core\View::getAssets("icon_stat.png")?> alt="">Statistiques</h1>
+	<div class="d-flex-wrap" id="dashboard-section-statisticsContainer">
+		<article class="statisticsBasic">
+			<h1>Consultations</h1>
+			<div>
+				<h2 class="numberStat numberStat-negative">426</h2>
+				<p>Depuis 16 heures</p>
+				<p>Moyenne : 430</p>
+			</div>
+		</article>
+		<article class="statisticsBasic">
+			<h1>Réservations</h1>
+			<div>
+				<h2 class="numberStat numberStat-positive">28</h2>
+				<p>Depuis 16 heures</p>
+				<p>Moyenne : 26</p>
+			</div>
+		</article class="statisticsBasic">
+		<article class="statisticsBasic">
+			<h1>Ventes</h1>
+			<div>
+				<h2 class="numberStat numberStat-positive">427</h2>
+				<p>Depuis 16 heures</p>
+				<p>Moyenne : 361</p>
+			</div>
+		</article>
+	</div>
+</section>
+
+<!-- Line 2 -->
+<section class="col-6" style="grid-column: 7 / 13; grid-row: 1;">
+	<h1 class="titleSection"><img src=<?php App\Core\View::getAssets("icon_alert.png")?> alt="">Alertes</h1>
+	<ul class="listItemComplete limit-height-125">
+		<li class="legend">
+			<p class="flex-weight-1">Libellé</p>
+			<p class="flex-weight-1">Niveau</p>
+			<p class="flex-weight-2">Description</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Contact</p>
+			<p class="flex-weight-1 status-color-important">Affichage</p>
+			<p class="flex-weight-2">Description</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Hôtel de Marseille</p>
+			<p class="flex-weight-1 status-color-normal">Réservation</p>
+			<p class="flex-weight-2">Description</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Accueil</p>
+			<p class="flex-weight-1 status-color-moyen">Affichage</p>
+			<p class="flex-weight-2">Description</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Configuration URL</p>
+			<p class="flex-weight-1 status-color-important">Configuration</p>
+			<p class="flex-weight-2">Description</p>
+			<p></p>
+		</li>
+	</ul>
+</section>
+
+<section class="col-8 multipleSectionContainer" style="grid-column: 1 / 9; grid-row: 2;">
+	<div class="multipleSection">
+		<h1 class="titleSection"><img src=<?php App\Core\View::getAssets("icon_plane.png")?> alt="">Voyages</h1>
+		<ul class="listItemBasic limit-height-300">
+			<li class="legend"><p>Informations</p><p>Inscrits</p></li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination</h2>
+				</div>
+				<p class="listItem-cpt">152/245</p>
+			</li>
+		</ul>
+	</div>
+	<div class="multipleSection">
+		<h1 class="titleSection"><img src=<?php App\Core\View::getAssets("icon_calendar.png")?> alt="">Réservations</h1>
+		<ul class="listItemBasic limit-height-300">
+			<li class="legend"><p>Informations</p><p>Places</p></li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">1</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">2</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">5</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">1</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">3</p>
+			</li>
+		</ul>
+	</div>
+	<div class="multipleSection">
+		<h1 class="titleSection"><img src=<?php App\Core\View::getAssets("icon_cancel.png")?> alt="">Annulations</h1>
+		<ul class="listItemBasic limit-height-300">
+			<li class="legend"><p>Informations</p><p>Places</p></li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">2</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">1</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">1</p>
+			</li>
+			<li class="listItem">
+				<div>
+					<p>12/12/2021</p>
+					<h2>Destination - Nom Prénom</h2>
+				</div>
+				<p class="listItem-cpt">3</p>
+			</li>
+		</ul>
+	</div>
+</section>
+
+<section class="col-12" style="grid-column: 9 / 13; grid-row: 2 / 4;">
+	<h1 class="titleSection"><img src=<?php App\Core\View::getAssets("icon_comment.png")?> alt="">Commentaires</h1>
+	<ul class="listItemComplete limit-height-450">
+		<li class="legend">
+			<p class="flex-weight-1">Utilisateurs</p>
+			<p class="flex-weight-1">Titres</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Contact</p>
+			<p class="flex-weight-1 status-color-important">Affichage</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Hôtel de Marseille</p>
+			<p class="flex-weight-1 status-color-normal">Réservation</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Accueil</p>
+			<p class="flex-weight-1 status-color-moyen">Affichage</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Configuration URL</p>
+			<p class="flex-weight-1 status-color-important">Configuration</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Contact</p>
+			<p class="flex-weight-1 status-color-important">Affichage</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Hôtel de Marseille</p>
+			<p class="flex-weight-1 status-color-normal">Réservation</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Accueil</p>
+			<p class="flex-weight-1 status-color-moyen">Affichage</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Configuration URL</p>
+			<p class="flex-weight-1 status-color-important">Configuration</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Contact</p>
+			<p class="flex-weight-1 status-color-important">Affichage</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Hôtel de Marseille</p>
+			<p class="flex-weight-1 status-color-normal">Réservation</p>
+			<p></p>
+		</li>
+		<li class="listItem">
+			<p class="flex-weight-1">Hôtel de Marseille</p>
+			<p class="flex-weight-1 status-color-normal">Réservation</p>
+			<p></p>
+		</li>
+	</ul>
+</section>
+
+<!-- Line 3 -->
+<section class="col-8" style="grid-column: 1 / 9; grid-row: 3;">
+	<h1 class="titleSection"><img src=<?php App\Core\View::getAssets("icon_quick_access.png")?> alt="">Accès rapides</h1>
+	<ul class="quickAccesContainer">
+		<li>
+			<p class="quickAccesTitle">Vols</p>
+			<img src=<?php App\Core\View::getAssets("icon_plane.png")?> alt="">
+		</li>
+		<li>
+			<p class="quickAccesTitle">Pages</p>
+			<img src=<?php App\Core\View::getAssets("icon_page.png")?> alt="">
+		</li>
+		<li>
+			<p class="quickAccesTitle">Articles</p>
+			<img src=<?php App\Core\View::getAssets("icon_book.png")?> alt="">
+		</li>
+		<li>
+			<p class="quickAccesTitle">Commentaires</p>
+			<img src=<?php App\Core\View::getAssets("icon_comment.png")?> alt="">
+		</li>
+		<li>
+			<p class="quickAccesTitle">Utilisateurs</p>
+			<img src=<?php App\Core\View::getAssets("icon_user.png")?> alt="">
+		</li>
+		<li>
+			<p class="quickAccesTitle">Statistiques</p>
+			<img src=<?php App\Core\View::getAssets("icon_stat.png")?> alt="">
+		</li>
+	</ul>
 </section>
