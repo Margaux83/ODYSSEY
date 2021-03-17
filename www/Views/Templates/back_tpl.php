9609573075f376b21bec988d9b8bab0b7d41f833
loudovic12 <loudovic.rexharrison@gmail.com>
2021-03-17 12:49:42 +0100

    Fix AddUser Pages

--- www/Views/Templates/back_tpl.php
+++ www/Views/Templates/back_tpl.php
@@ -5,7 +5,6 @@
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Odyssey - <?= App\Core\View::getActualPageTitle($menuData, $actualUri) ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" type="text/css" href=<?php App\Core\View::getAssets("main.css")?>>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
