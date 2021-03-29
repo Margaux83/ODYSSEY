f98a65a8c71baff70ebab809a9e696c7f0f83aa4
Margaux83 <margauxhebert83@gmail.com>
2021-03-29 23:20:04 +0200

    Création de la page des commentaires
    - Ajout du controller Comment.php avec l'action dashboard
    - Ajout de la view Comment_view.php avec le tableau des commentaires
    - Ajout de la popup pour la confirmation de la suppression des commentaires, dans le fichier popups.js

--- www/public/scripts/js/popups.js
+++ www/public/scripts/js/popups.js
@@ -11,13 +11,34 @@ if(document.getElementById("ModalConfirmDeleteArticle") != null) {
     });
 }
 
+var ModalConfirmDeleteComment = document.getElementById("ModalConfirmDeleteComment");
+
+if(document.getElementById("ModalConfirmDeleteComment") != null) {
+// When the user clicks on the button, open the modal
+    $(".openModalConfirmDeleteComment").on('click', function () {
+        ModalConfirmDeleteComment.style.display = "block";
+    });
+    $(".closeModalDelete").on('click', function () {
+        ModalConfirmDeleteComment.style.display = "none";
+    });
+}
+
 window.onclick = function(event) {
     if (event.target == ModalConfirmDeleteArticle) {
         ModalConfirmDeleteArticle.style.display = "none";
     }
+    if (event.target == ModalConfirmDeleteComment) {
+        ModalConfirmDeleteComment.style.display = "none";
+    }
 }
 
 $(".openModalConfirmDeleteArticle").on('click',function () {
+    setTimeout(function () {
+        $(".check-icon").show();
+    }, 10000);
+});
+
+$(".openModalConfirmDeleteComment").on('click',function () {
     setTimeout(function () {
         $(".check-icon").show();
     }, 10000);
