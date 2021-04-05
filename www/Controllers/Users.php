304c2685c33d398ff81b4c1d6d417b3953da303c
loudovic12 <loudovic.rexharrison@gmail.com>
2021-04-05 20:09:57 +0200

    Update develop branch in my project
    Mise à jour de la branche feature-pages-user avec develop

--- www/Controllers/Users.php
+++ www/Controllers/Users.php
@@ -9,15 +9,15 @@ use App\Core\View;
 class Users{
 
 
-    public function defaultAction($menuData, $actualUri){
-        $view = new View("users", "back", $menuData, $actualUri);
+    public function defaultAction(){
+        $view = new View("users", "back");
 
 
     }
 
-    public function addUsersAction($menuData, $actualUri)
+    public function addUsersAction()
     {
-        $view = new View("addUsers", "back", $menuData, $actualUri);
+        $view = new View("addUsers", "back");
 
     }
 
