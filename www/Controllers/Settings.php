3270f5c49fc955ee5e212ebf56081d1d43129ae1
MoulinLouis <l.moulin@lprs.fr>
2021-04-27 16:55:31 +0200

    Settings action fix

--- www/Controllers/Settings.php
+++ www/Controllers/Settings.php
@@ -6,7 +6,7 @@ use App\Core\Security;
 use App\Core\View;
 
 Class Settings{
-    public function defaultAction($menuData, $actualUri){
-        $view = new View("settings", "back", $menuData, $actualUri);
+    public function defaultAction(){
+        $view = new View("settings", "back");
     }
 }
