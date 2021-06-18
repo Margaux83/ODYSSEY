a23ae34568f68316ab58633fe2dccbdb24931069
MoulinLouis <l.moulin@lprs.fr>
2021-06-18 20:24:02 +0200

    Init elements Media
    Création des vues, du controllers, des routes et du modèle pour les médias

---
+++ www/Controllers/Media.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use App\Core\Form;
+use App\Core\View;
+use App\Models\User;
+use App\Models\Media as ModelMedia;
+use App\Core\Helpers;
+
+class Media {
+    public function defaultAction() {
+
+    }
+
+    public function addMediaAction() {
+
+    }
+}
---
+++ www/Models/Media.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use App\Core\Database;
+use App\Core\Form;
+
+class Media extends Database {
+    public function __construct(){
+        parent::__construct();
+    }
+
+
+}
