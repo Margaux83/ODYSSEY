3d891b35b39470d716f2c1f51ce773fea5250dc0
Margaux83 <margauxhebert83@gmail.com>
2021-07-20 21:43:33 +0200

    fix de l'erreur au niveau de l'ajout des articles (il fallait retirer le required au textarea du TinyMCE

--- www/Views/Article/add_articles_view.php
+++ www/Views/Article/add_articles_view.php
@@ -13,7 +13,6 @@
         $("#uri").val(value.toLowerCase());
     }
 </script>
-<script src=<?php App\Core\View::getAssets("article.js")?>></script>
 
 <script src='https://cdn.tiny.cloud/1/sfgeuuulhp5vmw2y9c0fo94ydvh7zpah75c6trahqaw5g1y7/tinymce/5/tinymce.min.js' referrerpolicy="origin"></script>
 <script src=<?php App\Core\View::getAssets("TinyMCE/fr_FR.js")?>></script>
--- www/Views/Article/edit_articles_view.php
+++ www/Views/Article/edit_articles_view.php
@@ -1,8 +1,18 @@
-<section class="col-12" style="grid-column: 1 / 13; grid-row: 2;">
-       <?php  App\Core\Form::showForm($form); ?>
+
+
+<section class="col-12" style="grid-column: 1 / 13; grid-row: 1;">
+    <?php  App\Core\Form::showForm($form); ?>
 </section>
 
-
+<script>
+    $("#title").keyup(function(){
+        update();
+    });
+    function update() {
+        let value = $('#title').val().replace(/[^a-z0-9\w\d]+/g, "-");
+        $("#uri").val(value.toLowerCase());
+    }
+</script>
 <script src=<?php App\Core\View::getAssets("article.js")?>></script>
 
 <script src='https://cdn.tiny.cloud/1/sfgeuuulhp5vmw2y9c0fo94ydvh7zpah75c6trahqaw5g1y7/tinymce/5/tinymce.min.js' referrerpolicy="origin"></script>
