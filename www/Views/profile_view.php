48417ac0ffeb127308d66702b33c2a8d8fd78106
maxime carl <mcarluer@myges.fr>
2021-07-25 16:09:32 +0200

    Feature [Profile] - change password

--- www/Views/profile_view.php
+++ www/Views/profile_view.php
@@ -1,3 +1,11 @@
-<section style="grid-column: 1 / 13; grid-row: 1;">
+<section style="grid-column: 1 / 7; grid-row: 1; padding: 10px; padding-bottom: 30px;">
+    <h1>Informations générales du compte</h1>
     <?php  App\Core\Form::showForm($form); ?>
+</section>
+
+<section style="grid-column: 7 / 13; grid-row: 1; padding: 10px;">
+    <h1>Modifier votre mot de passe</h1>
+    <div>
+        <?php  App\Core\Form::showForm($formPassword); ?>
+    </div>
 </section>
